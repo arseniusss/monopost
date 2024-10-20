fa6a4887a9c7217202bf8d69e01ef503a87766db
midnightpretenders <125821824+midnightpretenders@users.noreply.github.com>
2024-10-20 18:13:21 +0300

    Add files via upload

---
+++ Monopost.BLL/Services/Interfaces/IUserPersonalInfoManagementService.cs
@@ -0,0 +1,9 @@
+﻿using Monopost.BLL.Models;
+
+namespace Monopost.BLL.Services.Interfaces
+{
+    public interface IUserPersonalInfoManagementService
+    {
+        Task<Result> UpdateUserPersonalInfoAsync(UserPersonalInfoModel model);
+    }
+}
