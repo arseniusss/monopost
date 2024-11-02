0c1d793b477974acebabc3e234acfa873ee1bbcd
ch1kkapo0m <161013186+ch1kkapo0m@users.noreply.github.com>
2024-11-02 18:39:07 +0200

    UI

--- Monopost.Web/MainWindow.xaml.cs
+++ Monopost.Web/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Monopost.Web.ViewModels;
 
-namespace Monopost.Web
+namespace Monopost.Web.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var mainViewModel = new MainViewModel { MainFrame = MainFrame };
+            DataContext = mainViewModel;
         }
     }
 }
