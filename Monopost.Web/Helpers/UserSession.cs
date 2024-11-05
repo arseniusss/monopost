81cb210a616a9a765cc232f9c9aeafd052dfa9bd
ch1kkapo0m <161013186+ch1kkapo0m@users.noreply.github.com>
2024-11-05 10:59:49 +0200

    'stable' version

---
+++ Monopost.Web/Helpers/UserSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopost.PresentationLayer.Helpers
+{
+    public static class UserSession
+    {
+       public static int CurrentUserId { get; set; } = 0; 
+      
+    }
+
+}
